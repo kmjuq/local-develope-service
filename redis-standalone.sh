bd58cf1f5c9e8d8884fcfc60d69c92574c80391d
柯梦剑 <mengjian.ke@hand-china.com>
2023-10-14 10:24:57 +0800

    修改端口，添加注意事项

--- redis-standalone.sh
+++ redis-standalone.sh
@@ -2,7 +2,7 @@
 # 参考链接 https://hub.docker.com/r/redis/redis-stack
 docker run -d \
 --name redis-stack \
--p 6379:6379 \
--p 7902:8001 \
+-p 50300:6379 \
+-p 50390:8001 \
 -e REDIS_ARGS="--requirepass 123456" \
 redis/redis-stack:latest
